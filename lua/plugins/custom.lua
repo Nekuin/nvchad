b445fc76e931d6c0dd2e76b6c3769acd78f6c2d8
Nekuin <nekuin@gmail.com>
2024-09-24 14:37:27 +0300

    conditional copilot config

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -6,11 +6,14 @@ local plugins = {
     "github/copilot.vim",
     lazy = false,
     config = function()
+      local is_windows = package.config:sub(1,1) == '\\'
       -- Mapping tab is already used by NvChad
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_assume_mapped = true
       vim.g.copilot_tab_fallback = ""
-      vim.g.copilot_node_command = "/home/simo/.nvm/versions/node/v18.16.0/bin/node"
+      if not is_windows then
+        vim.g.copilot_node_command = "/home/simo/.nvm/versions/node/v18.16.0/bin/node"
+      end
       -- The mapping is set to other key, see custom/lua/mappings
       -- or run <leader>ch to see copilot mapping section
     end,
