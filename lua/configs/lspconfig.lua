a807278d52e8d1ffc62a22d8ed53747663faeacc
simo <nekuin@gmail.com>
2025-02-24 10:54:34 +0200

    ruby_lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "clangd", "phpactor", "ts_ls", "eslint" }
+local servers = { "html", "cssls", "clangd", "phpactor", "ts_ls", "eslint", "ruby_lsp" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
